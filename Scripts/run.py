31d95095c300fa66a256c38ea475a058e741d36a
Tushar Chenan <tusky2002@gmail.com>
2021-08-22 19:27:49 +0530

    move from run to os.system

--- Scripts/run.py
+++ Scripts/run.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
 
-from subprocess import run
 from blessings import Terminal
 from time import time
 from os import system
@@ -13,23 +12,18 @@ MAIN_CODE_FILE = MAIN_CODE_BASE_NAME + '.' + MAIN_CODE_EXTENSION
 term = Terminal()
 
 # Function to execute command locally
-def exec(cmd, cwd="."):
-    try:
-        res = run(cmd, cwd=cwd, capture_output=True, check=True)
-        return (True, res.stdout.decode('utf-8'))
-    except:
-        res = run(cmd, cwd=cwd, capture_output=True)
-        return (False, res.stderr.decode('utf-8'))
+def exec(cmd):
+    res = system(cmd)
+    return res == 0
 
 # Compile main code file
 def compile():
     st = time()
-    executed, res = exec(["g++", "-std=c++17", "-O2", MAIN_CODE_FILE, "-o", MAIN_CODE_BASE_NAME])
+    executed = exec(f"g++ -std=c++17 -O2 {MAIN_CODE_FILE} -o {MAIN_CODE_BASE_NAME}")
     et = time()
 
     if not executed:
         print(term.bold(term.red("[FAIL] Compilation failed")))
-        print(term.red("Compile Error:\n"), res)
         exit(0)
 
     print(term.bold(term.green("[OK] Compiled successfully")))
@@ -38,26 +32,27 @@ def compile():
 
 def main():
     compile()
+
     test_at = 1
     while True:
         try:
             with open(f"in{test_at}", "r") as file:
                 inData = file.read().strip()
 
-            ok = system(f"./{MAIN_CODE_BASE_NAME} < in{test_at} > {MAIN_CODE_BASE_NAME}out{test_at}")
-            if(ok != 0):
+            with open(f"out{test_at}", "r") as file:
+                correctOutput = file.read().strip()
+
+            ok = exec(f"./{MAIN_CODE_BASE_NAME} < in{test_at} > {MAIN_CODE_BASE_NAME}out{test_at}")
+            if not ok:
                 print(term.bold(term.red(f"[RTE] Run Time Error on Test {test_at}")))
                 print(term.bold(term.red("Test Data:")))
                 print(inData)
                 exit(0)
 
-            with open(f"out{test_at}", "r") as file:
-                correctOutput = file.read().strip()
-
             with open(f"{MAIN_CODE_BASE_NAME}out{test_at}", "r") as file:
                 programOutput = file.read().strip()
 
-            system(f"rm {MAIN_CODE_BASE_NAME}out{test_at}")
+            exec(f"rm {MAIN_CODE_BASE_NAME}out{test_at}")
 
             if correctOutput == programOutput:
                 print(term.bold(term.green(f"[AC] Test {test_at}")))
@@ -75,7 +70,7 @@ def main():
 
         except Exception:
             test_at -= 1
-            print(term.bold(term.green(f"[OK] {test_at}/{test_at} test cases")))
+            print(term.bold(term.green(f"[OK] {test_at} test cases")))
             break
 
     return 0
