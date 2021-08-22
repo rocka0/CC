21e780c8575a68b27d163ecad45cef349d1d724b
Tushar Chenan <tusky2002@gmail.com>
2021-08-22 18:01:23 +0530

    remove newlines

--- Scripts/run.py
+++ Scripts/run.py
@@ -26,10 +26,12 @@ def compile():
     st = time()
     executed, res = exec(["g++", "-std=c++17", "-O2", MAIN_CODE_FILE, "-o", MAIN_CODE_BASE_NAME])
     et = time()
+
     if not executed:
         print(term.bold(term.red("[FAIL] Compilation failed")))
         print(term.red("Compile Error:\n"), res)
         exit(0)
+
     print(term.bold(term.green("[OK] Compiled successfully")))
     delta = str(round(et-st, 3)) + 's'
     print(term.blue("Compile Time: "), term.bold(term.green((delta))))
@@ -40,28 +42,37 @@ def main():
     while True:
         try:
             with open(f"in{test_at}", "r") as file:
-                inData = file.read()
+                inData = file.read().strip()
+
             ok = system(f"./{MAIN_CODE_BASE_NAME} < in{test_at} > {MAIN_CODE_BASE_NAME}out{test_at}")
             if(ok != 0):
                 print(term.bold(term.red(f"[RTE] Run Time Error on Test {test_at}")))
                 print(term.bold(term.red("Test Data:")))
                 print(inData)
                 exit(0)
+
             with open(f"out{test_at}", "r") as file:
                 correctOutput = file.read().strip()
+
             with open(f"{MAIN_CODE_BASE_NAME}out{test_at}", "r") as file:
                 programOutput = file.read().strip()
+
             system(f"rm {MAIN_CODE_BASE_NAME}out{test_at}")
+
             if correctOutput == programOutput:
                 print(term.bold(term.green(f"[AC] Test {test_at}")))
             else:
                 print(term.bold(term.red(f"[WA] Wrong Answer on Test {  test_at}")))
+                print(term.bold(term.blue("Test Data:")))
+                print(inData)
                 print(term.bold(term.green("Expected Answer:")))
                 print(correctOutput)
                 print(term.bold(term.red("Your Answer:")))
                 print(programOutput)
                 exit(0)
+
             test_at += 1
+
         except Exception:
             test_at -= 1
             print(term.bold(term.green(f"[OK] {test_at}/{test_at} test cases")))
