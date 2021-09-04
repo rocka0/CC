3cb7baad74224a100cacb06175c41b99277b8fd1
Tushar Chenan <tusky2002@gmail.com>
2021-09-05 01:34:38 +0530

    Change template

--- Snippets/template.cpp
+++ Snippets/template.cpp
@@ -1,5 +1,3 @@
-// ᕙ(⇀‸↼‶)ᕗ ~ TBC
-
 // Header Files
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -19,21 +17,22 @@ using namespace __gnu_pbds;
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 
 // Typedefs
-using ll = long long;
-using ull = unsigned long long;
+using ll = int64_t;
+using ull = uint64_t;
 using ld = long double;
 using vi = vector<int>;
 using pi = pair<int, int>;
 template <typename T>
-using ordered_set = tree<
-    T,
-    null_type,
-    less<T>,
-    rb_tree_tag,
-    tree_order_statistics_node_update>;
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // Debugging
-string to_string(string s)
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+string to_string(const string& s)
 {
     return '"' + s + '"';
 }
@@ -45,10 +44,31 @@ string to_string(bool b)
 {
     return (b ? "true" : "false");
 }
-template <typename A, typename B>
-string to_string(pair<A, B> p)
+string to_string(vector<bool> v)
 {
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < sz(v); i++)
+    {
+        if (!first)
+        {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+template <size_t N>
+string to_string(bitset<N> v)
+{
+    string res = "";
+    for (size_t i = 0; i < N; i++)
+    {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
 }
 template <typename A>
 string to_string(A v)
@@ -66,6 +86,21 @@ string to_string(A v)
     }
     res += "}";
     return res;
+}
+template <typename A, typename B>
+string to_string(pair<A, B> p)
+{
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p)
+{
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p)
+{
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
 }
 void debug_out()
 {
@@ -144,7 +179,7 @@ you must cross,
 the stream of life,
 no one,
 but you yourself alone.
-    - Friedrich Wilhelm Nietzsche
+	- Friedrich Wilhelm Nietzsche
 */
 
 int main()
