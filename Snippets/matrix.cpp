01e5e58dd0c3561bfab54775332c802def343de9
Tushar Chenan <tusky2002@gmail.com>
2021-09-08 12:36:01 +0530

    Move from clangformat to sublimeAstyleformat

--- Snippets/matrix.cpp
+++ Snippets/matrix.cpp
@@ -1,100 +1,92 @@
 // https://github.com/rocka0/CC
 
 template <typename T>
-class Matrix
-{
-    vector<vector<T>> mat;
+class Matrix {
+        vector<vector<T>> mat;
 
-public:
-    int row_size;
-    int col_size;
+    public:
+        int row_size;
+        int col_size;
 
-    Matrix(int R, int C)
-    {
-        assert(R > 0);
-        assert(C > 0);
-        row_size = R;
-        col_size = C;
-        mat.resize(R);
-        for (int i = 0; i < row_size; ++i)
-            mat[i].resize(C);
-    }
+        Matrix ( int R, int C ) {
+            assert ( R > 0 );
+            assert ( C > 0 );
+            row_size = R;
+            col_size = C;
+            mat.resize ( R );
 
-    vector<T>& operator[](int x)
-    {
-        assert(x < row_size);
-        return mat[x];
-    }
-
-    const vector<T>& operator[](int x) const
-    {
-        assert(x < row_size);
-        return mat[x];
-    }
-
-    Matrix add(const Matrix& A, const Matrix& B)
-    {
-        assert(A.row_size == B.row_size);
-        assert(A.col_size == B.col_size);
-        Matrix ans(A.row_size, B.col_size);
-        for (int i = 0; i < A.row_size; ++i)
-        {
-            for (int j = 0; j < B.col_size; ++j)
-            {
-                ans[i][j] = A[i][j] + B[i][j];
-            }
+            for ( int i = 0; i < row_size; ++i )
+            { mat[i].resize ( C ); }
         }
-        return ans;
-    }
 
-    Matrix subtract(const Matrix& A, const Matrix& B)
-    {
-        assert(A.row_size == B.row_size);
-        assert(A.col_size == B.col_size);
-        Matrix ans(A.row_size, B.col_size);
-        for (int i = 0; i < A.row_size; ++i)
-        {
-            for (int j = 0; j < B.col_size; ++j)
-            {
-                ans[i][j] = A[i][j] - B[i][j];
-            }
+        vector<T>& operator[] ( int x ) {
+            assert ( x < row_size );
+            return mat[x];
         }
-        return ans;
-    }
 
-    Matrix multiply(const Matrix& A, const Matrix& B)
-    {
-        assert(A.col_size == B.row_size);
-        Matrix ans(A.row_size, B.col_size);
-        for (int i = 0; i < A.row_size; ++i)
-        {
-            for (int j = 0; j < B.col_size; ++j)
-            {
-                for (int k = 0; k < A.col_size; ++k)
-                {
-                    ans[i][j] += (A[i][k] * B[k][j]);
+        const vector<T>& operator[] ( int x ) const {
+            assert ( x < row_size );
+            return mat[x];
+        }
+
+        Matrix add ( const Matrix& A, const Matrix& B ) {
+            assert ( A.row_size == B.row_size );
+            assert ( A.col_size == B.col_size );
+            Matrix ans ( A.row_size, B.col_size );
+
+            for ( int i = 0; i < A.row_size; ++i ) {
+                for ( int j = 0; j < B.col_size; ++j ) {
+                    ans[i][j] = A[i][j] + B[i][j];
                 }
             }
-        }
-        return ans;
-    }
 
-    Matrix power(const Matrix& M, ll p)
-    {
-        assert(p >= 1);
-        if (p == 1)
-        {
-            return M;
+            return ans;
         }
-        else
-        {
-            Matrix H = power(M, p >> 1);
-            H = multiply(H, H);
-            if (p & 1)
-            {
-                H = multiply(H, M);
+
+        Matrix subtract ( const Matrix& A, const Matrix& B ) {
+            assert ( A.row_size == B.row_size );
+            assert ( A.col_size == B.col_size );
+            Matrix ans ( A.row_size, B.col_size );
+
+            for ( int i = 0; i < A.row_size; ++i ) {
+                for ( int j = 0; j < B.col_size; ++j ) {
+                    ans[i][j] = A[i][j] - B[i][j];
+                }
             }
-            return H;
+
+            return ans;
         }
-    }
+
+        Matrix multiply ( const Matrix& A, const Matrix& B ) {
+            assert ( A.col_size == B.row_size );
+            Matrix ans ( A.row_size, B.col_size );
+
+            for ( int i = 0; i < A.row_size; ++i ) {
+                for ( int j = 0; j < B.col_size; ++j ) {
+                    for ( int k = 0; k < A.col_size; ++k ) {
+                        ans[i][j] += ( A[i][k] * B[k][j] );
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        Matrix power ( const Matrix& M, ll p ) {
+            assert ( p >= 1 );
+
+            if ( p == 1 ) {
+                return M;
+            }
+            else {
+                Matrix H = power ( M, p >> 1 );
+                H = multiply ( H, H );
+
+                if ( p & 1 ) {
+                    H = multiply ( H, M );
+                }
+
+                return H;
+            }
+        }
 };
--- Snippets/sieve.cpp
+++ Snippets/sieve.cpp
@@ -1,38 +1,35 @@
 // https://github.com/the-hyp0cr1t3/CC
 
 const int nax = 2e7;
-array<int, nax + 1> spf;
+array < int, nax + 1 > spf;
 vi primes;
 
 void initSieve()
 {
-    iota(all(spf), 0);
+    iota ( all ( spf ), 0 );
     // There are 1270607 primes less than 2e7
-    primes.reserve(1270607);
-    primes.pb(2);
-    for (int i = 4; i <= nax; i += 2)
-    {
+    primes.reserve ( 1270607 );
+    primes.pb ( 2 );
+
+    for ( int i = 4; i <= nax; i += 2 ) {
         spf[i] = 2;
     }
-    for (int i = 3; i * i <= nax; i += 2)
-    {
-        if (spf[i] == i)
-        {
+
+    for ( int i = 3; i * i <= nax; i += 2 ) {
+        if ( spf[i] == i ) {
             int inc = 2 * i;
-            for (int j = i * i; j <= nax; j += inc)
-            {
-                if (spf[j] == j)
-                {
+
+            for ( int j = i * i; j <= nax; j += inc ) {
+                if ( spf[j] == j ) {
                     spf[j] = i;
                 }
             }
         }
     }
-    for (int i = 3; i <= nax; i += 2)
-    {
-        if (spf[i] == i)
-        {
-            primes.pb(i);
+
+    for ( int i = 3; i <= nax; i += 2 ) {
+        if ( spf[i] == i ) {
+            primes.pb ( i );
         }
     }
 }
