e4fee80cac6a9437a97319b7a42971a6453212aa
Tushar <tusky2002@gmail.com>
2021-09-15 23:55:11 +0530

    Update template

--- Snippets/template.cpp
+++ Snippets/template.cpp
@@ -39,92 +39,45 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 // Debugging
 template <typename A, typename B>
-string to_string(pair<A, B> p);
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p);
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p);
-string to_string(const string& s) { return '"' + s + '"'; }
-string to_string(const char* s) { return to_string((string)s); }
-string to_string(bool b) { return (b ? "true" : "false"); }
-string to_string(vector<bool> v)
+ostream& operator<<(ostream& os, const pair<A, B>& p)
 {
-    bool first = true;
-    string res = "{";
-    for (int i = 0; i < sz(v); i++) {
-        if (!first) {
-            res += ", ";
-        }
-        first = false;
-        res += to_string(v[i]);
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+template < typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type >
+ostream & operator<<(ostream& os, const T_container& v)
+{
+    os << '{';
+    string sep;
+    for (const T& x : v) {
+        os << sep << x, sep = ", ";
     }
-    res += "}";
-    return res;
-}
-template <size_t N>
-string to_string(bitset<N> v)
-{
-    string res;
-    for (size_t i = 0; i < N; i++) {
-        res += static_cast<char>('0' + v[i]);
-    }
-    return res;
-}
-template <typename A>
-string to_string(A v)
-{
-    bool first = true;
-    string res = "{";
-    for (const auto& x : v) {
-        if (!first) {
-            res += ", ";
-        }
-        first = false;
-        res += to_string(x);
-    }
-    res += "}";
-    return res;
-}
-template <typename A, typename B>
-string to_string(pair<A, B> p)
-{
-    return "(" + to_string(p.ff) + ", " + to_string(p.ss) + ")";
-}
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p)
-{
-    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
-}
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p)
-{
-    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+    return os << '}';
 }
 void debug_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T)
 {
-    cerr << " " << to_string(H);
+    cerr << ' ' << H;
     debug_out(T...);
 }
 
 // Recursive Lambda
-template<class Fun>
+template <class Fun>
 class y_combinator_result {
     public:
-        template<class T>
-        explicit y_combinator_result(T&& fun): fun_(std::forward<T>(fun)) {}
-        template<class ...Args>
-        decltype(auto) operator()(Args&& ...args) {
-            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        template <class T>
+        explicit y_combinator_result(T&& fun): fun_(forward<T>(fun)) {}
+        template <class... Args>
+        decltype(auto) operator()(Args&& ... args) {
+            return fun_(ref(*this), forward<Args>(args)...);
         }
     private:
         Fun fun_;
 };
-template<class Fun>
+template <class Fun>
 decltype(auto) y_combinator(Fun&& fun)
 {
-    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    return y_combinator_result<decay_t<Fun> >(forward<Fun>(fun));
 }
 
 // Change max/min functions
