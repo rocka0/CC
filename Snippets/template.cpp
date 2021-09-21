6446ee5102ac2b0e0296779fa94dc0eaa862a862
Tushar <tusky2002@gmail.com>
2021-09-21 22:26:50 +0530

    Update template

--- Snippets/template.cpp
+++ Snippets/template.cpp
@@ -21,7 +21,6 @@ using namespace std;
 using namespace __gnu_pbds;
 
 // Macros
-#define all(x) ((x)).begin(), ((x)).end()
 #define sz(x) static_cast<int>((x).size())
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 
@@ -35,22 +34,65 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 // Debugging
 template <typename A, typename B>
-ostream& operator<<(ostream& os, const pair<A, B>& p) {
-    return os << '(' << p.first << ", " << p.second << ')';
-}
-template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
-ostream& operator<<(ostream& os, const T_container& v) {
-    os << '{';
-    string sep;
-    for (const T& x : v) {
-        os << sep << x, sep = ", ";
+string to_string(pair<A, B> p);
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+string to_string(const string& s) { return '"' + s + '"'; }
+string to_string(const char* s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
     }
-    return os << '}';
+    res += "}";
+    return res;
+}
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
 }
 void debug_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) {
-    cerr << ' ' << H;
+    cerr << " " << to_string(H);
     debug_out(T...);
 }
 
