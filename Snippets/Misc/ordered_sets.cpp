fe9d2f8c0a616fd877d1ead85a0f31831abc3e04
Tushar <tusky2002@gmail.com>
2021-12-31 11:21:59 +0530

    Add ordered multiset using pairs

--- Snippets/Misc/ordered_sets.cpp
+++ Snippets/Misc/ordered_sets.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
     Source: https://codeforces.com/blog/entry/11080
 
-    Ordered_Set:
+    Ordered Set:
         Usage:  OrderedSet<int> oset;
                 oset.insert(1);
                 oset.insert(2);
@@ -31,19 +31,26 @@ using namespace std;
                 the second template argument type must be declared as mapped type,
                 instead of null_type.
 
-    OrderedMultiset:
-        Usage:  OrderedMultiset<int> omset;
-
         Note:   One can achieve OrderedMultiset using ordered_set by changing
                 less<T> to less_equal<T>.
-                There are some things to be noted about this OrderedMultiset:
-                    1. OrderedMultiset::erase() method doesn't work
-                    2. OrderedMultiset::lower_bound() behaves like upper_bound()
-                    3. OrderedMultiset::upper_bound() behaves like lower_bound()
-                    4. OrderedMultiset::find() always returns OrderedMultiset::end()
-                    5. OrderedMultiset::find_by_order() and OrderedMultiset::order_of_key()
-                    work normally
 
+                Usage:  template <typename T>
+                        using OrderedMultiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+                        OrderedMultiset<int> omset;
+
+                        There are some things to be noted about this OrderedMultiset:
+                            1. OrderedMultiset::erase() method doesn't work
+                            2. OrderedMultiset::lower_bound() behaves like upper_bound()
+                            3. OrderedMultiset::upper_bound() behaves like lower_bound()
+                            4. OrderedMultiset::find() always returns OrderedMultiset::end()
+                            5. OrderedMultiset::find_by_order() and OrderedMultiset::order_of_key()
+                            work normally
+
+    Ordered Multiset [built using pair<T,int>]:
+        Usage:  OrderedMultiset<int> omset;
+                omset.insert(3);
+                assert(omset.erase(3));
 */
 
 #include <ext/pb_ds/assoc_container.hpp>
@@ -54,4 +61,43 @@ template <typename T>
 using OrderedSet = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 template <typename T>
-using OrderedMultiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct OrderedMultiset {
+    OrderedSet<pair<T, int>> data;
+    map<T, int> freq;
+
+    void insert(T x) {
+        data.insert({x, freq[x]});
+        ++freq[x];
+    };
+
+    bool erase(T x) {
+        if (freq.count(x) and freq[x] > 0) {
+            --freq[x];
+            auto it = data.find({x, freq[x]});
+            data.erase(it);
+            if (!freq[x]) {
+                freq.erase(x);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int strictlyLess(T x) {
+        return data.order_of_key({x, 0});
+    }
+
+    int strictlyEqual(T x) {
+        return freq[x];
+    }
+
+    int strictlyGreater(T x) {
+        return (int) data.size() - strictlyLess(x) - strictlyEqual(x);
+    }
+
+    T getNthElement(int n) {
+        assert(0 <= n and n < (int) data.size());
+        return data.find_by_order(n)->first;
+    }
+};
