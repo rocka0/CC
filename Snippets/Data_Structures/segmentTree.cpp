0d6fee3fbaf397df3b38b651bbff0ac2c1c16d39
Tushar <tusky2002@gmail.com>
2022-03-06 09:42:15 +0530

    Update segment tree snippet

--- Snippets/Data_Structures/segmentTree.cpp
+++ Snippets/Data_Structures/segmentTree.cpp
@@ -4,63 +4,68 @@ using namespace std;
 /*
     Source: https://github.com/rocka0/CC
 
-    Usage:  SegmentTree segt(vector<SegmentTree::Node> arr);
-            segt.update(int index, SegmentTree::Node val)
-            segt.query(int l, int r) -> returns SegmentTree::Node
-            of (op) on range[l, r)
+    Usage:  SegmentTree seg(vector<int> arr);
+            seg.update(int index, int val)
+            seg.query(int l, int r) -> returns associative op on range[l, r)
 */
+
+// TODO: Change Node of SegmentTree
+struct Node {
+    using T = int64_t;
+
+    T val;
+
+    // TODO: Change default constructor to reflect Identity Node
+    Node() : Node(INT_MAX) {}
+
+    Node(T val) {
+        this->val = val;
+    }
+
+    operator T() const {
+        return val;
+    }
+};
+
+const Node IdentityNode;
+
+// TODO: Change Associative Merge Operation of SegmentTree
+Node merge(const Node &left, const Node &right) {
+    return Node(min(left.val, right.val));
+}
 
 class SegmentTree {
 public:
-    // Segment Tree Node
-    struct Node {
-        int64_t val;
-
-        Node() : Node(0) {}
-        Node(int64_t val) {
-            this->val = val;
-        }
-    };
-
-private:
-    // Associative merge operation
-    Node merge(const Node &left, const Node &right) {
-        Node res(left.val + right.val);
-        return res;
-    }
-
-    // Identity Node
-    const Node ID;
-
-public:
     SegmentTree() : SegmentTree(0) {}
-    SegmentTree(int n) : SegmentTree(vector<Node>(n, ID)) {}
-    SegmentTree(const vector<Node> &arr) {
+    SegmentTree(int n) : SegmentTree(vector<int>(n, 0)) {}
+
+    SegmentTree(const vector<int> &arr) {
         int _n = arr.size();
         int log = ceil_pow2(_n);
         n = 1 << log;
-        tree.resize(n << 1, ID);
+        tree.resize(n << 1, IdentityNode);
         for (int i = 0; i < _n; i++) {
-            tree[n + i] = arr[i];
+            // TODO: Set leaf nodes of SegmentTree from arr
+            tree[n + i] = Node(arr[i]);
         }
         for (int i = n - 1; i >= 1; i--) {
             pull(i);
         }
     }
 
-    void update(int index, Node val) {
-        tree[index += n] = val;
+    void update(int index, int val) {
+        tree[index += n] = Node(val);
         while (index > 1) {
             index >>= 1;
             pull(index);
         }
     }
 
-    Node query(int l, int r, int at, int lo, int hi) {
+    Node query(int l, int r, int at, int lo, int hi) const {
         if (l <= lo and hi <= r) {
             return tree[at];
         } else if (hi <= l or r <= lo) {
-            return ID;
+            return IdentityNode;
         } else {
             int mi = (lo + hi) >> 1;
             Node left = query(l, r, at << 1, lo, mi);
@@ -69,7 +74,7 @@ public:
         }
     }
 
-    Node query(int l, int r) {
+    Node query(int l, int r) const {
         return query(l, r, 1, 0, n);
     }
 
