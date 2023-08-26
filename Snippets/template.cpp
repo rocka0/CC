fd99f40028e769e5020b0e412b2b485ad38ad42e
Tushar Chenan <tusky2002@gmail.com>
2023-08-26 11:53:27 +0530

    Update template

--- Snippets/template.cpp
+++ Snippets/template.cpp
@@ -10,14 +10,14 @@ template <typename T>
 bool chmax(T &x, T y) {
     return x < y and (x = y, true);
 }
+
 template <typename T>
 bool chmin(T &x, T y) {
     return x > y and (x = y, true);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
 
-    // Code goes here
+    // The Legend Himself
 }
