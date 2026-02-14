27f5e054d7e62d58014fcb1f3636dee2669dac0d
Tushar Chenan <tusky2002@gmail.com>
2026-02-15 03:18:39 +0530

    feat: make array hashing more robust

--- Snippets/Misc/arr_hash.cpp
+++ Snippets/Misc/arr_hash.cpp
@@ -1,13 +1,21 @@
-#include <array>
 #include <cassert>
 #include <chrono>
 #include <cstdint>
+#include <iostream>
 #include <random>
 #include <utility>
 #include <vector>
 
-using mint = Z<1000000007>;
+template <int MOD>
+struct Z;
 
+constexpr int MOD1 = 1000000007;
+constexpr int MOD2 = 1000000009;
+
+using Z1 = Z<MOD1>;
+using Z2 = Z<MOD2>;
+
+template <typename mint>
 struct array_hash {
     int n;
     std::vector<mint> p, pref;
@@ -16,7 +24,7 @@ struct array_hash {
 
     array_hash(const std::vector<int>& arr, mint base) : n(arr.size()), p(n + 1, 1), pref(n + 1, 0) {
         for (int i = 0; i < n; ++i) p[i + 1] = p[i] * base;
-        for (int i = 0; i < n; ++i) pref[i + 1] = pref[i] * base + arr[i];
+        for (int i = 0; i < n; ++i) pref[i + 1] = pref[i] * base + mint(arr[i]);
     }
 
     mint get(int l, int r) const {
@@ -25,25 +33,26 @@ struct array_hash {
     }
 };
 
-template <int K = 2>
 struct robust_array_hash {
-    static inline std::array<mint, K> bases;
+    static inline Z1 base1;
+    static inline Z2 base2;
     static inline bool bases_init = false;
-    std::array<array_hash, K> hashes;
+
+    array_hash<Z1> hash1;
+    array_hash<Z2> hash2;
 
     explicit robust_array_hash(const std::vector<int>& arr) {
         if (!bases_init) {
             std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-            std::uniform_int_distribution<int> dist(257, MOD - 1);
-            for (int i = 0; i < K; ++i) bases[i] = dist(rng);
+            std::uniform_int_distribution<int> dist1(257, MOD1 - 2);
+            std::uniform_int_distribution<int> dist2(257, MOD2 - 2);
+            base1 = Z1(dist1(rng));
+            base2 = Z2(dist2(rng));
             bases_init = true;
         }
-        for (int i = 0; i < K; ++i) hashes[i] = array_hash(arr, bases[i]);
+        hash1 = array_hash<Z1>(arr, base1);
+        hash2 = array_hash<Z2>(arr, base2);
     }
 
-    std::array<mint, K> get(int l, int r) const {
-        std::array<mint, K> res;
-        for (int i = 0; i < K; ++i) res[i] = hashes[i].get(l, r);
-        return res;
-    }
+    std::pair<Z1, Z2> get(int l, int r) const { return {hash1.get(l, r), hash2.get(l, r)}; }
 };
